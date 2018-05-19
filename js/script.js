460e103956cbdee44b5d8f7cf8ab1333d3cf535c
lazarengaba <ngabalazare3@gmail.com>
2018-05-19 16:30:27 +0000

    Premiere version du script personnalise

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.discoverBtn').click(function() {
+		$(this).hide();
+		$('.plusOptionVoiture').hide();
+		$('.deplierBtn').show();
+		//$('html,body').animate({scrollTop:600}, 1200);
+		$('#bodyTop').slideUp(1200);
+		$('.header').slideUp(1200);
+		$('.middleBar').slideUp(1200);
+		$('#bodyBottom').show();
+	});
+	$('.btn.btn-info.btn-lg.deplierBtn').click(function() {
+		$(this).hide();
+		$('.plusOptionVoiture').show();
+		$('#bodyBottom').slideUp(1200);
+		$('.discoverBtn').show();
+		$('#bodyTop').slideDown(1200);
+		$('.header').slideDown(1200);
+		$('.middleBar').slideDown(1200);
+	});
+});
