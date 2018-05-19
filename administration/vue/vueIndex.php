6d3e8b893d61a86f44f9f91cd0ee55f815efc451
lazarengaba <ngabalazare3@gmail.com>
2018-05-19 16:10:49 +0000

    Premiere version vue administrateur

---
+++ administration/vue/vueIndex.php
@@ -0,0 +1,67 @@
+<div class="coverAdminArea">
+	<div class="middleBar rmUnderline">
+		<table width="100%">
+			<tr>
+				<td width="200">
+					<i class="fa fa-car fa-2x"> </i>
+					CarsLocation
+				</td>
+				<td align="right" class="slogan">
+					<a href="#" style="color: #fff;">
+						<i class="fa fa-question-circle"> </i>
+						Obtenir de l'aide
+					</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div class="container-slideLeft rmUnderline">
+		<div class="row">
+			<div class="col-xs-3">
+				<div class="slideLeft">
+					<div class="dashboard">
+						<i class="fa fa-dashboard"> </i>
+						<b>Tableau de bord</b>
+					</div>
+					<a href="#">
+						<div class="slideLeftItems">
+							Ajouter un véhicule
+							<i class="fa fa-chevron-right pull-right" style="line-height: 40px;"> </i>
+						</div>
+					</a>
+					<a href="#">
+						<div class="slideLeftItems">
+							Modifer un véhicule
+							<i class="fa fa-chevron-right pull-right" style="line-height: 40px;"> </i>
+						</div>
+					</a>
+					<a href="#">
+						<div class="slideLeftItems">
+							<i class="fa fa-eye"> </i>
+							Afiicher les véhicules
+							<i class="fa fa-chevron-right pull-right" style="line-height: 40px;"> </i>
+						</div>
+					</a>
+					<a href="#">
+						<div class="slideLeftItems">
+							<i class="fa fa-trash"> </i>
+							Supprimer un véhicule
+							<i class="fa fa-chevron-right pull-right" style="line-height: 40px;"> </i>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-xs-9">
+				<div class="dashboardBody"><br /><br /><br />
+					<center>
+						<h1 style="color: #aaa;">
+							<i class="fa fa-wrench fa-4x"> </i>
+							<i class="fa fa-cog fa-4x"> </i><br /><br />
+							Espace de configuration administrateur
+						</h1>
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
