6fed91673357ebad2a4516f6e5eebfac6cc9fcc4
lazarengaba <ngabalazare3@gmail.com>
2018-05-19 15:59:54 +0000

    Premiere version a uploader

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="CSS3 Lightbox - A CSS-only lightbox" />
+	<title>Accueil</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+
+</head>
+<body id="page">
+
+	<?php
+		require 'vue/vueIndex.php';
+	?>
+
+
+
+	<script type="text/javascript" src="js/jquery compressed-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
