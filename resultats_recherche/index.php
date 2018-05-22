8b91ee7935c4cd2b201aab04c43e45f7cbd31bd0
lazarengaba <ngabalazare3@gmail.com>
2018-05-22 14:17:45 +0000

    Premiere version page resultats recherche

---
+++ resultats_recherche/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="CSS3 Lightbox - A CSS-only lightbox" />
+	<title>Résultats Recherche</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css" />
+	<link rel="stylesheet" type="text/css" href="../css/font-awesome-4.7.0/css/font-awesome.min.css" />
+</head>
+<body>
+
+	<?php
+		require 'vue/vueResultatsRecherche.php';
+	?>
+
+	<script type="text/javascript" src="../js/jquery compressed-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/script.js"></script>
+
+</body>
+</html>
