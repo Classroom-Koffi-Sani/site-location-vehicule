3929ce4f66ab91006c4c74542426f5d2d91c37a4
lazarengaba <ngabalazare3@gmail.com>
2018-05-22 09:57:08 +0000

    premiere version requetes ajax admin

---
+++ ajax/modifier_vehicule.php
@@ -0,0 +1,222 @@
+<center>
+	<h3><i class="fa fa-pencil"> </i> Modifier un véhicule</h3>
+	<div style="border: 1px solid #aaa; margin-right: 40px;"></div>
+</center><br />
+
+<div style="width: 60%;padding-left: 15px;">
+	<table width="100%">
+		<tr>
+			<td width="240">
+				Chercher un véhicule à modifier <i class="glyphicon glyphicon-triangle-right"> </i>
+			</td>
+			<td>
+				<form id="search" method="post" action="">
+							<div class="input-group">
+								<input type="text" name="motCle" id="motCle" class="form-control" />
+								<span class="input-group-btn">
+									<button type="submit" class="btn btn-danger" style="width: 60px;">
+										<span class="glyphicon glyphicon-search"></span>
+									</button>
+								</span>
+							</div>
+						</form>
+			</td>
+		</tr>
+	</table>
+</div><br />
+
+<div style="padding-right: 30px;">
+<div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+<div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div><div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+
+<div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+
+<div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+<div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div><div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+
+</div><div class="col-xs-3">
+	<img class="img-rounded" src="../images/19190708_1559814290708870_841662609_n.jpg" width="100%" />
+
+	<br />
+	<div class="voitureDesc">
+		<div class="voitureDescAdmin">
+			<center><b>Volvo 145, 5 chaises</b></center>
+			Source d'énergie : Gaz-oil
+		</div>
+		<div style="height: 10px;"></div>
+		<center>
+			<a href="#" class="btn btn-info btn-sm">
+				<i class="glyphicon glyphicon-eye-open"> </i>
+				<b>Afficher</b>
+			</a>
+			<a href="#" class="btn btn-default btn-sm">
+				<i class="fa fa-pencil"> </i>
+				<b>Modifier</b>
+			</a>
+		</center>
+	</div>
+
+</div>
+
+
+
+
+</div>
+<a href="#" class="btn btn-primary btn-sm" style="margin-left: 15px;">
+	<i class="fa fa-plus-circle"> </i>
+	Voir plus
+</a>
