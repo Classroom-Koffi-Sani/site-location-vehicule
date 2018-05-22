5dca304d155aae4009bc0b4e7297633789a92647
lazarengaba <ngabalazare3@gmail.com>
2018-05-22 14:18:34 +0000

    Version intermediaire controlleur principal

---
+++ resultats_recherche/controleur/controlRecherche.php
@@ -0,0 +1,23 @@
+<?php
+	/**
+	* 
+	*/
+	class SearchResult
+	{
+		//private $motCle;
+
+		public function Recherche($motCle) {
+			if (isset($motCle)&&!empty($motCle)) {
+				die("ok");
+			} else {
+				# code...
+			}
+			
+		}
+		
+		function __construct()
+		{
+			# code...
+		}
+	}
+?>
